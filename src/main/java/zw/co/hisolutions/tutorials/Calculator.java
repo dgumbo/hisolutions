7e06bc12dbd39e4fe1dbd16972f0999cb1789dce
Denzil Gumbo <gdenzilg@gmail.com>
2018-03-06 12:48:45 +0200

    Master Reset Commit

--- src/main/java/zw/co/hisolutions/tutorials/Calculator.java
+++
@@ -1,7 +0,0 @@
-package zw.co.hisolutions.tutorials;
-
-public class Calculator {
-    public int abs(int val){
-        return val < 0 ? -val : val ;
-    }
-}
--- src/main/java/zw/co/hisolutions/tutorials/services/util/DBActionResult.java
+++
@@ -1,5 +0,0 @@
-package zw.co.hisolutions.tutorials.services.util;
-
-public enum DBActionResult {
-    Success, Failed, EncounteredError, PrimaryKeyViolation, ForeignKeyViolation, UniqueConstraintViolation
-}
