a9180d38bfbf695fa8b951d61c0d91f9abaa129c
dgumbo <gdenzilg@gmail.com>
2018-06-19 18:06:25 +0200

    created core module successifully

---
+++ core/src/main/java/zw/co/hisolutions/common/CoreModuleApplication.java
@@ -0,0 +1,12 @@
+package zw.co.hisolutions.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication
+public class CoreModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreModuleApplication.class, args);
+    } 
+}
--- core/src/main/java/zw/co/hisolutions/common/dao/GenericDao_1.java
+++ core/src/main/java/zw/co/hisolutions/common/dao/GenericDao_1.java
@@ -1,22 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package zw.co.hisolutions.common.dao;
-
-import java.io.Serializable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository; 
-import zw.co.hisolutions.common.entity.BaseEntity;
-
-/**
- *
- * @author denzil
- * @param <T> 
- * @param <ID> 
- */
-@Repository
-public interface GenericDao<T extends BaseEntity, ID extends Serializable>  extends JpaRepository<T, Long>{  
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package zw.co.hisolutions.common.dao;
+//
+//import java.io.Serializable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository; 
+//import zw.co.hisolutions.common.entity.BaseEntity;
+//
+///**
+// *
+// * @author denzil
+// * @param <T> 
+// * @param <ID> 
+// */
+//@Repository
+//public interface GenericDao<T extends BaseEntity, ID extends Serializable>  extends JpaRepository<T, Long>{  
+//    
+//}
