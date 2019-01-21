32f4dd495365ed6aa129dddfe42a29367172eae1
dgumbo <gdenzilg@gmail.com>
2019-01-21 09:22:45 +0200

    Updated Price Update Code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,47 +1,51 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';  
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {AuthGuard} from 'auth/guards';
 
 const routes: Routes = [
-  {
-    path: 'welcome',
-    loadChildren: './welcome/welcome.module#WelcomeModule'
-  },
-   
-  {
-    path: 'admin', 
-    loadChildren: './admin/admin.module#AdminModule', 
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'login',
-    redirectTo: '/auth/login',
-    pathMatch: 'full',  
-  },
-  {
-    path: 'logout',
-    redirectTo: '/auth/logout',
-    pathMatch: 'full',  
-  },
-  {
-    path: 'auth', 
-    loadChildren: './auth/auth.module#AuthModule'
-  },
-  {
-    path: 'services', 
-    loadChildren: './products-and-services/products-and-services.module#ProductsAndServicesModule'
-  },
-  {
-    path: '',
-    redirectTo: 'welcome',
-    pathMatch: 'full'
-  }
+    //  {
+    //    path: '',
+    //    redirectTo: 'welcome',
+    //    pathMatch: 'full'
+    //  },
+    {
+        path: '',
+        loadChildren: './welcome/welcome.module#WelcomeModule'
+    },
+    {
+        path: 'welcome',
+        loadChildren: './welcome/welcome.module#WelcomeModule'
+    },
+
+    {
+        path: 'admin',
+        loadChildren: './admin/admin.module#AdminModule',
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'login',
+        redirectTo: '/auth/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'logout',
+        redirectTo: '/auth/logout',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        loadChildren: './auth/auth.module#AuthModule'
+    },
+    {
+        path: 'services',
+        loadChildren: './products-and-services/products-and-services.module#ProductsAndServicesModule'
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
